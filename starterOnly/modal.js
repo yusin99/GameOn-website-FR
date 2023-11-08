b40d306b3a40516581e32af0aaa3be6b4cd6a6c4
Yusin <sunkatabeckama@gmail.com>
2023-11-08 14:57:35 +0100

    Extract DOM elements for future manipulation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,14 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const checkboxPlace = document.querySelectorAll(".cb-place");
+const accepted = document.getElementById("checkbox1");
+const submit = document.querySelector(".submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
